f1c8c172bb763b2f0d4d1f96bba968879e29a20b
ldiebold <luke@ldiebold.com>
2020-02-22 14:34:48 +1100

    Initial Commit

---
+++ frontend/client/src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="q-pa-xl">
+    <div class="row">
+      <q-input v-model="form.api_key" label="Xero Key" />
+    </div>
+
+    <div>{{ form.api_key }}</div>
+
+    <div class="row q-mt-lg">
+      <q-input v-model="form.api_secret" label="Xero Secret" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data() {
+    return {
+      form: {
+        api_key: '',
+        api_secret: '',
+      }
+    }
+  }
+}
+</script>
